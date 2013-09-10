a63f177444d12188513f49fa7a853655b1cf8054
Jean <jean@feltmeng.com>
2013-09-10 14:33:35 +0800

    add splash

--- src/com/gabriel/pgap22/star/Star_gabriel_pgap22.java
+++ src/com/gabriel/pgap22/star/Star_gabriel_pgap22.java
@@ -28,7 +28,8 @@ public class Star_gabriel_pgap22 extends DroidGap{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 //        super.init();
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html", 10000);
         
         	
         
